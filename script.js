bed2aa7c9e8dbbcede232e6d0756b1b3813a6d6a
Vitor6671 <vitor.gabriel.perin@escola.pr.gov.br>
2025-09-22 10:46:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ campoResultado.textContent = palavras.join(" , ");
 }
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras){
+        frequencias [i] = 0;
+
+        for (let j of palavras){
+            if ( i == j){
+        frequencias[i]++;
+    }
+        }
+    }
+console.log(frequencias);
     return palavras;
 }
